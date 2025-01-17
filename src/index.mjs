3f0cd58abfd2410bdcdfd2e32358be9e409ce7e5
barry hykes Jr <barryhykesjr@barrys-MacBook-Air.local>
2025-01-17 14:28:29 -0600

    App Security Enhancement Logic

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 // Load environment variables
 import "./loadEnvironment.js";
+import helmet from "helmet";
 import express from 'express'; 
 import bodyParser from 'body-parser';
 import connectDb from "./db/conn.js";
@@ -7,7 +8,7 @@ import serverless from 'serverless-http';
 import swaggerUi from  'swagger-ui-express';
 //import * as swaggerFile from './swagger_output.json'
 //const swaggerFile = require('./swagger_output.json');
-import { port, swaggerDocs } from "./config.js";
+import { appSecret, appSession, port, swaggerDocs } from "./config.js";
 import { rateLimiter } from "./middleware/rate-limiter.mjs";
 
 const app = express();
@@ -20,9 +21,29 @@ const app = express();
 //  optionsSuccessStatus: 200,
 //};
 //app.use(cors(corsOptions))  // can use as middleware on certain routes
+// some extra security
+app.use(session({
+  name: appSession,
+  secret: appSecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: true}  // Ensure cookies are only sent over HTTPS
+}))
+// Use Helmet for securing HTTP headers
+if (!isDev){
+  app.use(helmet()); 
+  app.use((req, res, next) => {  // redirect to https
+    if (!req.secure) {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 //app.use(bodyParser.json())
 /**
  * @swagger
@@ -43,6 +64,7 @@ app.get('/', rateLimiter, (req, res) => {
 import authRouter from "./routes/authRoutes.js";
 app.use('/', authRouter);
 import productRouter  from './routes/products.js';
+import session from "express-session";
 app.use("/products", productRouter);
 
 await connectDb()
